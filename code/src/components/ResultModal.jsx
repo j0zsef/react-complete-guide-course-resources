0df5ce1dce8217b8d47cd1e8914038b9070f01a2
Joe Nevin <jnevin@enova.com>
2023-11-30 15:05:37 -0600

    Complete refs & portals.

--- code/src/components/ResultModal.jsx
+++ code/src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 
 const ResultModal = forwardRef(function ResultModal(
   { result, targetTime, remainingTime, onReset },
@@ -14,10 +15,12 @@ const ResultModal = forwardRef(function ResultModal(
   });
   const userLost = remainingTime <= 0;
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
-  return (
+  return createPortal(
     <dialog ref={dialog} className="result-modal">
       {userLost && <h2>You Lost</h2>}
+      {!userLost && <h2>Score {score}</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
@@ -25,10 +28,11 @@ const ResultModal = forwardRef(function ResultModal(
         You stopped the time with
         <strong> {formattedRemainingTime} seconds left.</strong>
       </p>
-      <form method="dialog" onSubmit={onReset}>
+      <form method="dialog" onSubmit={onReset} onClose={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal"),
   );
 });
 
