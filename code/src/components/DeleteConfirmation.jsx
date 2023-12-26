c64b9ca2b5004a2261d45d2781b5374870bcb1e4
Joe Nevin <jnevin@enova.com>
2023-12-26 15:03:31 -0600

    Complete course.

--- code/src/components/DeleteConfirmation.jsx
+++ code/src/components/DeleteConfirmation.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+import ProgressBar from "./ProgressBar.jsx";
+const TIMER = 3000;
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      onConfirm();
+    }, TIMER);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [onConfirm]);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
@@ -11,6 +25,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <ProgressBar timer={TIMER} />
     </div>
   );
 }
